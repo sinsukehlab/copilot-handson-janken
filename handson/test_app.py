09565dccdead072950a4703c7c7e45eb1709ab9a
sinsukehlab <94058150+sinsukehlab@users.noreply.github.com>
2024-03-09 15:44:01 +0900

    Refactor test cases for app.main() function

--- handson/test_app.py
+++ handson/test_app.py
@@ -4,26 +4,25 @@ import app
 
 class TestApp(unittest.TestCase):
 
-    @patch('app.input', return_value='0')
-    @patch('app.random.randint', return_value=1)
-    def test_main_win(self, input, randint):
-        with patch('app.print') as mocked_print:
-            app.main()
-            mocked_print.assert_any_call('あなたの勝ち')
+    def test_cases(self):
+        outcomes = {
+            (0, 0): 'あいこ',
+            (0, 1): 'あなたの勝ち',
+            (0, 2): 'あなたの負け',
+            (1, 0): 'あなたの負け',
+            (1, 1): 'あいこ',
+            (1, 2): 'あなたの勝ち',
+            (2, 0): 'あなたの勝ち',
+            (2, 1): 'あなたの負け',
+            (2, 2): 'あいこ'
+        }
 
-    @patch('app.input', return_value='0')
-    @patch('app.random.randint', return_value=0)
-    def test_main_draw(self, input, randint):
-        with patch('app.print') as mocked_print:
-            app.main()
-            mocked_print.assert_any_call('あいこ')
-
-    @patch('app.input', return_value='0')
-    @patch('app.random.randint', return_value=2)
-    def test_main_lose(self, input, randint):
-        with patch('app.print') as mocked_print:
-            app.main()
-            mocked_print.assert_any_call('あなたの負け')
+        for (input_value, random_value), outcome in outcomes.items():
+            with patch('app.input', return_value=str(input_value)), \
+                 patch('app.random.randint', return_value=random_value), \
+                 patch('builtins.print') as mocked_print:
+                app.main()
+                mocked_print.assert_any_call(outcome)
 
 if __name__ == '__main__':
     unittest.main()
